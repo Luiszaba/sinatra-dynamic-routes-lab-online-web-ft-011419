c717232504335a8421ee081fa7046c53cd4d9d2d
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 01:02:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
   # Write your code here!
   
   get '/reversename/:name' do
-    @user_name = params[:name].reverse
+    user_name = params[:name].reverse
   end
 
   get '/square/:number' do
-    @square_this_number = params[:number].to_i ** 2
-    @square_this_number.to_s
+    square_this_number = params[:number].to_i ** 2
+    square_this_number.to_s
   end
   
   get '/say/:number/:phrase' do
