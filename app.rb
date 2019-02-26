6f51be4e77bcd7f0bc5cccf29b34073c3b9a422b
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 01:08:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     return_value
   end  
   
-  get '/say/:word1/:word3:word4/:word5' do 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
