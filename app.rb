c1475ec66af23bd9c7ef3d7f9b186d3bc21b13b0
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 01:26:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,15 +25,18 @@ class App < Sinatra::Base
   end  
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    
     word1 = params[:word1]
     word2 = params[:word2]
     word3 = params[:word3]
     word4 = params[:word4]
     word5 = params[:word5]
+    
     "#{word1} #{word2} #{word3} #{word4} #{word5}."
   end
     
   get '/:operation/:number1/:number2' do 
+    
     ops = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
