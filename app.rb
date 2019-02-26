16d812b4b54952210b2e08572d73421483a8855e
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 01:07:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{word1} #{word2} #{word3} #{word4} #{word5}
+    "#{word1} #{word2} #{word3} #{word4} #{word5}"
     end
     
   
@@ -40,4 +40,5 @@ class App < Sinatra::Base
   
   
   
+end
 end
